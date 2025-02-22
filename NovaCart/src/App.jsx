31fc146c7bfc78120909049fca898d38c488f940
QUASAR <mukungavi344@gmail.com>
2025-02-22 14:35:49 +0300

    Resolved issues with vite

--- NovaCart/src/App.jsx
+++ NovaCart/src/App.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './Dashboard.jsx';
@@ -10,21 +9,28 @@ import Related from './Related.jsx';
 import AboutUs from './AboutUs.jsx';
 import Footer from './Footer.jsx';
 
+function MainContent() {
+  return (
+    <>
+      <Dashboard />
+      <HomePage />
+      <CardCarousel />
+      <Products />
+      <Related />
+      <AboutUs />
+      <Footer />
+    </>
+  );
+}
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signin" element={<SignInPage />} />
-        <Route path="/stories" element={<CardCarousel />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/related" element={<Related />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/contact" element={<Footer />} />
+        <Route path="/" element={<MainContent />} />
+        <Route path="/SignInPage.jsx" element={<SignInPage />} />
       </Routes>
     </Router>
-
   );
 }
 
